013da337c91a87a860bbf753cfa8c744ea95b27d
Quang Pham <phvuquang@gmail.com>
2024-06-01 08:25:38 +0700

    refactor: optimization

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
+import { Box } from '@/components/ui/box'
 </script>
 
 <template>
-  <div>
-    <Button>Click me</Button>
-  </div>
+  <Box variant="red" />
 </template>
